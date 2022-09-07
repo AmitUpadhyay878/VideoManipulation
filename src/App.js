35842e793c3ea9450ad151f9cbfe5bc2773108a4
AMIT UPADHYAY <amitupadhyay878@gmail.com>
2022-09-07 17:20:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import MainFile from "./videoediting/mainfile";
+import Main from "./videojs/main";
+
+function App() {
+  return <Main />;
+}
+
+export default App;
